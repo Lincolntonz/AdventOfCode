e5f1183b6859514f772a65a26c5e26e5a0991a47
Ethan Smith <wayoffreedom@gmail.com>
2025-12-02 10:26:52 -0500

    feat: 2025 Day 2 parts 1 and 2 added.

--- AdventOfCode/2025Day1.cs
+++ AdventOfCode/2025Day1.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-///Advent of Code 2025
 //int dial = 50;  //starting dial value
 //int countZeros = 0; //day1 count anytime dial lands on zero
 //int countWraps = 0; //day2 count anytime dial pass zero (wraps around below 0 or above 99)
@@ -90,73 +89,83 @@ string Part2Original(string[] inputs)
 
 
 
+using AdventOfCode;
 
-
-string[] inputs = System.IO.File.ReadAllLines("C:\\CURRENT\\Testbed\\AdventOfCode\\AdventOfCode\\day1puzzleinput.txt");
-
-string[] inputs2 = {
-"R50"
-,"R50"
-,"L50"
-,"L50"
-,"R75"
-,"L50"
-,"L25"
-,"L75"
-,"R50" };
-
-
-
-
-
-
-///part 1 and 2 taken from a reddit solution. can't get my part 2 to work.
-string Part1(string[] input)
+///Advent of Code 2025
+internal class Program
 {
-    var d = 50;
-    const int maxD = 100;
-    var a = 0;
-    foreach (var line in input)
+    private static void Main(string[] args)
     {
-        // Determine direction of move. Right positive, left negative
-        var v = line[0] == 'R' ? 1 : -1;
-        //Parse move value
-        if (!int.TryParse(line[1..], out var c)) throw new Exception("Invalid number detected!");
-        //dial  is updated by value (subtracted or added based on direction)
-        d += (v * c);
-        //modulo max dial value to get the current dial position
-        d %= maxD;
-        //if dial position is a negative value, add max dial value to wrap around
-        if (d < 0) d += maxD;
-        //if dial is at zero, increment counter 
-        if (d == 0) a++;
+        //string[] inputs = File.ReadAllLines("C:\\CURRENT\\Testbed\\AdventOfCode\\AdventOfCode\\2025day1input.txt");
+
+        //string[] inputs2 = {
+        //                    "R50"
+        //                    ,"R50"
+        //                    ,"L50"
+        //                    ,"L50"
+        //                    ,"R75"
+        //                    ,"L50"
+        //                    ,"L25"
+        //                    ,"L75"
+        //                    ,"R50" };
+
+
+        _2025Day2 solver = new _2025Day2();
+        solver.aoc2025Day2();
+
+
+
+
+
+
+
     }
 
-    return "" + a;
-}
-
- 
-string Part2(string[] input)
-{
-    var d = 50;
-    const int maxD = 100;
-    var a = 0; //zero points
-    foreach (var line in input)
+    ///part 1 and 2 taken from a reddit solution. can't get my part 2 to work.
+    public string Part1(string[] input)
     {
-        var v = line[0] == 'R' ? 1 : -1; //direction
-        if (!int.TryParse(line[1..], out var c)) throw new Exception("Invalid number detected!");
-        var distToZero = v == 1 ? maxD - d : d; //distance to zero based on direction
-        if (distToZero > 0 && c >= distToZero) a++; //if distance value c, is greater than the distance to zero, we will pass zero at least once.
-        a += (c - distToZero) / maxD; //add any additional full wraps past zero to zeros counter
-        d += (v * c); //update dial position
-        d %= maxD; //modulo max dial value to get the current dial position        
-        if (d < 0) d += maxD;//if dial position is a negative value, add max dial value to wrap around
+        var d = 50;
+        const int maxD = 100;
+        var a = 0;
+        foreach (var line in input)
+        {
+            // Determine direction of move. Right positive, left negative
+            var v = line[0] == 'R' ? 1 : -1;
+            //Parse move value
+            if (!int.TryParse(line[1..], out var c)) throw new Exception("Invalid number detected!");
+            //dial  is updated by value (subtracted or added based on direction)
+            d += v * c;
+            //modulo max dial value to get the current dial position
+            d %= maxD;
+            //if dial position is a negative value, add max dial value to wrap around
+            if (d < 0) d += maxD;
+            //if dial is at zero, increment counter 
+            if (d == 0) a++;
+        }
 
-        Console.WriteLine(String.Format("Move: {0}  New Dial: {1}  Total Zeros: {2}", line, d, a));
+        return "" + a;
+    }
+    public string Part2(string[] input)
+    {
+        var d = 50;
+        const int maxD = 100;
+        var a = 0; //zero points
+        foreach (var line in input)
+        {
+            var v = line[0] == 'R' ? 1 : -1; //direction
+            if (!int.TryParse(line[1..], out var c)) throw new Exception("Invalid number detected!");
+            var distToZero = v == 1 ? maxD - d : d; //distance to zero based on direction
+            if (distToZero > 0 && c >= distToZero) a++; //if distance value c, is greater than the distance to zero, we will pass zero at least once.
+            a += (c - distToZero) / maxD; //add any additional full wraps past zero to zeros counter
+            d += v * c; //update dial position
+            d %= maxD; //modulo max dial value to get the current dial position        
+            if (d < 0) d += maxD;//if dial position is a negative value, add max dial value to wrap around
+
+            Console.WriteLine(string.Format("Move: {0}  New Dial: {1}  Total Zeros: {2}", line, d, a));
+        }
+
+        return "" + a;
     }
 
-    return "" + a;
+
 }
-
-
-Console.WriteLine(Part2(inputs2));
---
+++ AdventOfCode/2025Day2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public class _2025Day2
+    {
+
+        public Int64 aoc2025Day2()
+        {
+            //puzzle input is single line, comma separated ranges.
+            //each range is an starting rStart separated by a '-' followed by another string rEnd
+
+            int invalidIDCount = 0;
+
+            string inputPath = "C:\\CURRENT\\Testbed\\AdventOfCode\\AdventOfCode\\2025day2input.txt";
+
+            string[] inputs = System.IO.File.ReadAllLines("C:\\CURRENT\\Testbed\\AdventOfCode\\AdventOfCode\\2025day2input.txt")[0].Split(',');
+
+            Int64 invalidCount = 0;
+            Int64 invalidAmount = 0;
+            //InvalidProcessingPart1(ref invalidIDCount, inputs, ref invalidAmount);
+            InvalidProcessingPart2(ref invalidIDCount, inputs, ref invalidAmount);
+
+
+            return invalidAmount;
+
+        }
+
+        private static void InvalidProcessingPart1(ref int invalidIDCount, string[] inputs, ref long invalidAmount)
+        {
+            foreach (string range in inputs)
+            {
+                string[] bounds = range.Split('-');
+                Int64 rStart = Int64.Parse(bounds[0]);
+                Int64 rEnd = Int64.Parse(bounds[1]);
+
+                //Console.WriteLine(String.Format("{0}-{1}", rStart, rEnd));
+
+                List<Int64> invalidIds = new List<Int64>();
+                //Process each range here
+                for (Int64 i = rStart; i <= rEnd; i++)
+                {
+                    //i is considered invalid if it contains a sequence of digits repeated twice.
+                    if (Regex.IsMatch(i.ToString(), @"^(.+)\1$"))
+                    {
+                        invalidIds.Add(i);
+                        invalidIDCount++;
+                        invalidAmount += i;
+                    }
+                }
+
+                if (invalidIds.Count > 0)
+                {
+
+                    Console.WriteLine(String.Format("{0}-{1}\t\t\t\t\t\tInvalid IDs found: {2}", rStart, rEnd, string.Join(", ", invalidIds)));
+
+                    invalidIDCount += invalidIds.Count;
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("{0}-{1}", rStart, rEnd));
+                }
+            }
+
+            Console.WriteLine(String.Format("Total Invalid IDs found: {0}\t Sum of invalidIds: {1}", invalidIDCount, invalidAmount));
+        }
+
+        private static void InvalidProcessingPart2(ref int invalidIDCount, string[] inputs, ref long invalidAmount)
+        {
+            foreach (string range in inputs)
+            {
+                string[] bounds = range.Split('-');
+                Int64 rStart = Int64.Parse(bounds[0]);
+                Int64 rEnd = Int64.Parse(bounds[1]);
+
+                //Console.WriteLine(String.Format("{0}-{1}", rStart, rEnd));
+
+                List<Int64> invalidIds = new List<Int64>();
+                //Process each range here
+                for (Int64 i = rStart; i <= rEnd; i++)
+                {
+                    //For part 2. i is invalid if it contains a sequence repeated >= twice
+                    if (Regex.IsMatch(i.ToString(), @"^(.+)\1{1,}$"))
+                    {
+                        invalidIds.Add(i);
+                        invalidIDCount++;
+                        invalidAmount += i;
+                    }
+                }
+
+                if (invalidIds.Count > 0)
+                {
+
+                    Console.WriteLine(String.Format("{0}-{1}\t\tInvalid IDs found: {2}", rStart, rEnd, string.Join(", ", invalidIds)));
+
+                    invalidIDCount += invalidIds.Count;
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("{0}-{1}", rStart, rEnd));
+                }
+            }
+
+            Console.WriteLine(String.Format("Total Invalid IDs found: {0}\t Sum of invalidIds: {1}", invalidIDCount, invalidAmount));
+        }
+
+
+
+
+    }
+}
