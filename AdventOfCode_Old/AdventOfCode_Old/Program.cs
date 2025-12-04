34257a2307af4679b15feb2d8fe4506339ff2d33
Ethan Smith <wayoffreedom@gmail.com>
2025-12-04 16:14:52 -0500

    Day 6 parts 1 and 2

--- AdventOfCode_Old/AdventOfCode_Old/Program.cs
+++ AdventOfCode_Old/AdventOfCode_Old/Program.cs
@@ -1,99 +1,203 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-///////////
-/// 
-/// Advent of Code 2015 - Day 5
+
+
+
+
 using System.Text.RegularExpressions;
+//////////////////
+/// ///////
+/// aoc 2015 day 6.
+string[] inputs = System.IO.File.ReadAllLines(@"C:\CURRENT\Testbed\AdventOfCode\AdventOfCode_Old\AdventOfCode_Old\inputs.txt");
 
-//string input ="kjasfdlkajsfmxiusafjadfiejwp";
-string[] input = System.IO.File.ReadAllLines(@"C:\CURRENT\Testbed\AdventOfCode\AdventOfCode_Old\AdventOfCode_Old\inputs.txt");
+//create grid of lights and initialize all to 0.
+//, multidimensional array
+//part 1, lights can be 0 or 1.
+//bool[,] lights = new bool[1000,1000];
+
+//part 2, lights have brightness need to be int.
+int[,] lights = new int[1000,1000];
+
+const int TURNOFF = 0;
+const int TURNON  = 1;
+const int TOGGLE  = 2;
 
 
-var vowels = new HashSet<char>{'a','e','i','o','u'};
-int niceStrings = 0;
-foreach(string line in input)
+foreach(string instr in inputs)
 {
-    //niceStrings = Part1NiceString(vowels, niceStrings, line);
-    niceStrings = Part2NiceString(niceStrings, line);
-}
-
-Console.WriteLine("Total Nice Strings: " + niceStrings);
-
-static int Part2NiceString(int niceStrings, string line)
-{
-    bool niceString = false;
-    bool hasRepeatSequence = false;
-    bool hasRepeatSandwich = false;
-    Match repeatSequence = null;
-    Match repeatSandiwch = null;
-
-    //part 2:
-    //any two letters that repeat at least twice.
-    //contains a letter that repeats with one letter sandwiched between them.
-    //answer incorrect. original repeat regex:
-//"(.{2}).*(\1)"
-
-    //match repeat sequence of two letters, no overlap.
-    if ((repeatSequence = Regex.Match(line, @"(.{2}).*(\1)")).Success)
+    //quickest way I can think of to parse the coords. assumes no mistakes in input.
+    var coordinates = Regex.Matches(instr, @"\d+,\d+");
+    int[] startCoord = new int[]{int.Parse(coordinates[0].Value.Split(',')[0]), int.Parse(coordinates[0].Value.Split(',')[1])};
+    int[] endCoord = new[]{int.Parse(coordinates[1].Value.Split(',')[0]), int.Parse(coordinates[1].Value.Split(',')[1])};
+    //Console.WriteLine(String.Format(@"Instruction raw: {0} StartCoordinate = {1} End Coordinate: {2}", instr,startCoord[0]+" " +startCoord[1], endCoord[0] + " " + endCoord[1]));
+    //set range  to true
+    //feel like there's a neat way to turn on or off or toggle in one loop.
+    int instrOperation = ParseOp(instr);
+    //loop through given range performing the given operation.
+    for(int x = startCoord[0]; x <= endCoord[0]; x++)
     {
-        hasRepeatSequence = true;
-    }
-    //match letter sandwich. 
-    if ((repeatSandiwch = Regex.Match(line, @"(.).(\1)")).Success)
-    {
-        hasRepeatSandwich = true;
-    }
-    if (hasRepeatSandwich && hasRepeatSequence)
-    {
-        //Console.WriteLine("has enough vowels and a double letter and no illegals");
-        niceStrings++;
-        niceString = true;
-    }
-    Console.WriteLine(String.Format("Line: {0} Nice:{4} HasRepeatSequence: {1} {5} HasRepeatSandwich: {2} {6}", line, hasRepeatSequence, hasRepeatSandwich, repeatSequence.Value, niceString, repeatSequence.Value, repeatSandiwch.Value));
-    
-    return niceStrings;
-}
-
-
-
-
-
-
-static int Part1NiceString(HashSet<char> vowels, int niceStrings, string line)
-{
-    int vowelCount = 0;
-    bool hasDoubleLetter = false;
-    bool containsIllegal = false;
-    bool niceString = false;
-    Match illegalMatch = null;
-    Match doubleLetterMatch = null;
-    //loop through chars to count vowels.
-    foreach (char c in line)
-    {
-        if (vowels.Contains(c))
+        for(int y =startCoord[1]; y <= endCoord[1]; y++)
         {
-            vowelCount++;
+                switch (instrOperation)
+                {
+                    case TURNOFF:
+                    {
+                        //part 2 lower brightness by 1, 0 min.
+                        if (lights[x,y]> 0) lights[x,y]--;
+                        break;
+                    }
+                    case TURNON: //brightness+1
+                    {
+                        lights[x,y]++;
+                        break;
+                        
+                    }
+                    case TOGGLE: //brightness+2
+                    {
+                        lights[x,y]+=2;
+                        break;
+                        
+                    }
+                }
         }
     }
 
-    if ((doubleLetterMatch = Regex.Match(line, @"(.)\1")).Success)
-    {
-        hasDoubleLetter = true;
-    }
 
-    if ((illegalMatch = Regex.Match(line, @"ab|cd|pq|xy")).Success)
-    {
-        containsIllegal = true;
-    }
-    if (vowelCount >= 3 && hasDoubleLetter && !containsIllegal)
-    {
-        //Console.WriteLine("has enough vowels and a double letter and no illegals");
-        niceStrings++;
-        niceString = true;
-    }
-    Console.WriteLine(String.Format("Line: {0} Nice:{5} VowelCount: {1} HasDoubleLetter: {2} {6} ContainsIllegal: {3} {4}", line, vowelCount, hasDoubleLetter, containsIllegal, illegalMatch.Value, niceString, doubleLetterMatch.Value));
-    return niceStrings;
+
+
 }
+
+int lightsON = 0;
+int lightsTotalBrightness =0;
+for(int x = 0; x < lights.GetLength(0); x++)
+{
+    for(int y = 0; y < lights.GetLength(1); y++)
+    {
+        if(lights[x,y]>0){
+            // Console.ForegroundColor = (ConsoleColor)new Random().Next(1, 8); // Use 8 ANSI colors
+            // Console.Write('■');
+            // Console.ResetColor();
+            lightsON++;
+            lightsTotalBrightness+=lights[x,y];
+        }
+        else
+        {            
+            //Console.Write(' '); // Changed to space for off lights
+        }
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Lights ON: " + lightsON + "Total Brightness: " + lightsTotalBrightness);
+
+//parse operation
+int ParseOp(string instr)
+{
+    if (instr.StartsWith("turn on")) return TURNON;
+    if (instr.StartsWith("turn off")) return TURNOFF;
+    return TOGGLE;
+}
+
+
+///////////
+
+
+
+
+
+// ///////////
+// /// 
+// /// Advent of Code 2015 - Day 5
+// using System.Text.RegularExpressions;
+
+// //string input ="kjasfdlkajsfmxiusafjadfiejwp";
+// string[] input = System.IO.File.ReadAllLines(@"C:\CURRENT\Testbed\AdventOfCode\AdventOfCode_Old\AdventOfCode_Old\inputs.txt");
+
+
+// var vowels = new HashSet<char>{'a','e','i','o','u'};
+// int niceStrings = 0;
+// foreach(string line in input)
+// {
+//     //niceStrings = Part1NiceString(vowels, niceStrings, line);
+//     niceStrings = Part2NiceString(niceStrings, line);
+// }
+
+// Console.WriteLine("Total Nice Strings: " + niceStrings);
+
+// static int Part2NiceString(int niceStrings, string line)
+// {
+//     bool niceString = false;
+//     bool hasRepeatSequence = false;
+//     bool hasRepeatSandwich = false;
+//     Match repeatSequence = null;
+//     Match repeatSandiwch = null;
+
+//     //part 2:
+//     //any two letters that repeat at least twice.
+//     //contains a letter that repeats with one letter sandwiched between them.
+//     //answer incorrect. original repeat regex:
+// //"(.{2}).*(\1)"
+
+//     //match repeat sequence of two letters, no overlap.
+//     if ((repeatSequence = Regex.Match(line, @"(.{2}).*(\1)")).Success)
+//     {
+//         hasRepeatSequence = true;
+//     }
+//     //match letter sandwich. 
+//     if ((repeatSandiwch = Regex.Match(line, @"(.).(\1)")).Success)
+//     {
+//         hasRepeatSandwich = true;
+//     }
+//     if (hasRepeatSandwich && hasRepeatSequence)
+//     {
+//         //Console.WriteLine("has enough vowels and a double letter and no illegals");
+//         niceStrings++;
+//         niceString = true;
+//     }
+//     Console.WriteLine(String.Format("Line: {0} Nice:{4} HasRepeatSequence: {1} {5} HasRepeatSandwich: {2} {6}", line, hasRepeatSequence, hasRepeatSandwich, repeatSequence.Value, niceString, repeatSequence.Value, repeatSandiwch.Value));
+    
+//     return niceStrings;
+// }
+
+
+
+
+
+
+// static int Part1NiceString(HashSet<char> vowels, int niceStrings, string line)
+// {
+//     int vowelCount = 0;
+//     bool hasDoubleLetter = false;
+//     bool containsIllegal = false;
+//     bool niceString = false;
+//     Match illegalMatch = null;
+//     Match doubleLetterMatch = null;
+//     //loop through chars to count vowels.
+//     foreach (char c in line)
+//     {
+//         if (vowels.Contains(c))
+//         {
+//             vowelCount++;
+//         }
+//     }
+
+//     if ((doubleLetterMatch = Regex.Match(line, @"(.)\1")).Success)
+//     {
+//         hasDoubleLetter = true;
+//     }
+
+//     if ((illegalMatch = Regex.Match(line, @"ab|cd|pq|xy")).Success)
+//     {
+//         containsIllegal = true;
+//     }
+//     if (vowelCount >= 3 && hasDoubleLetter && !containsIllegal)
+//     {
+//         //Console.WriteLine("has enough vowels and a double letter and no illegals");
+//         niceStrings++;
+//         niceString = true;
+//     }
+//     Console.WriteLine(String.Format("Line: {0} Nice:{5} VowelCount: {1} HasDoubleLetter: {2} {6} ContainsIllegal: {3} {4}", line, vowelCount, hasDoubleLetter, containsIllegal, illegalMatch.Value, niceString, doubleLetterMatch.Value));
+//     return niceStrings;
+// }
 ///////////
 /// 
 /// 
